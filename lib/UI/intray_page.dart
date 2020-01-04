83f69c38594fa9551d91824721cdf66eed9285cd
kangsudal <kangsudal@gmail.com>
2020-01-04 14:55:40 +0900

    Create list for tasks #2

--- lib/UI/intray_page.dart
+++ lib/UI/intray_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/models/classes/task.dart';
 import 'package:todo_app/models/globa.dart';
 import 'package:todo_app/models/widgets/intray_todo_widget.dart';
 
@@ -8,22 +9,54 @@ class IntrayPage extends StatefulWidget {
 }
 
 class _IntrayPageState extends State<IntrayPage> {
+  List<Task> taskList = [];
   @override
   Widget build(BuildContext context) {
+    taskList = getList();
     return Container(
       color: coralColor,
-      child: ListView(
+      child: _buildReorderableList(context),
+    );
+  }
+
+  _buildReorderableList(BuildContext context) {
+    return Theme(
+      data: ThemeData(canvasColor: Colors.transparent),
+      child: ReorderableListView(
+        //드레그 가능한 리스트뷰
         padding: EdgeInsets.only(top: 300),
-        children: getList(),
+        children:
+            taskList.map((Task item) => _buildListTile(context, item)).toList(),
+        //taskList: 데이터들, _buildListTile 데이터 담는 포장용기
+        onReorder: _onReorder,
       ),
     );
   }
 
-  List<Widget> getList() {
-    List<IntrayTodo> _list = [];
-    for (int i = 0; i < 20; i++) {
-      _list.add(IntrayTodo(title:"Hello"));
+  void _onReorder(int oldIndex, int newIndex) {
+    setState(() {
+      // if (newIndex > oldIndex) {
+      //   newIndex -= 1;
+      // }
+      Task item = taskList[oldIndex];
+      taskList.remove(item);
+      taskList.insert(newIndex, item);
+    });
+  }
+
+  List<Task> getList() {
+    for (int i = 0; i < 10; i++) {
+      taskList.add(Task("My first todo " + i.toString(), false, i.toString()));
     }
-    return _list;
+    return taskList;
+  }
+
+  Widget _buildListTile(BuildContext context, Task item) {
+    return ListTile(
+      key: Key(item.taskId),
+      title: IntrayTodo(
+        title: item.title,
+      ),
+    );
   }
 }
